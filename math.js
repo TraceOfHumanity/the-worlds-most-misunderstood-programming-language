9cc43ec77d5050c2c1659f7212c1bec5594d5c85
Alexander_Ovchar <azalexxx02@gmail.com>
2024-05-20 00:48:11 +0300

    `chore: Update math.js with additional math functions`

--- math.js
+++ math.js
@@ -1,3 +1,5 @@
+const PI = 3.141592653589793;
+
 console.log(Math.pow(2, 3)); // 8
 console.log(Math.sqrt(16)); // 4
 console.log(Math.random());
@@ -7,4 +9,30 @@ console.log(Math.round(3.5)); // 4
 console.log(Math.max(1, 2, 3, 4, 5)); // 5
 console.log(Math.min(1, 2, 3, 4, 5)); // 1
 console.log(Math.PI); // 3.141592653589793
-console.log(Math.E); // 2.718281828459045
+console.log(Math.trunc(3.9)); // 3 
+console.log(PI.toFixed(2)); // 3.14
+console.log(Math.hypot(3, 4)); // 5
+console.log(Math.max(1, 2, 3, 4, 5)); // 5
+console.log(Math.min(1, 2, 3, 4, 5)); // 1
+console.log(Math.sin(PI / 2)); // 1
+console.log(Math.cos(PI)); // -1
+console.log(Math.tan(PI / 4)); // 1
+console.log(Math.asin(1)); // 1.570796326794896
+console.log(Math.acos(-1)); // 3.141592653589793
+console.log(Math.atan(1)); // 0.7853981633974483
+console.log(Math.sinh(1)); // 1.1752011936438014
+console.log(Math.cosh(1)); // 1.5430806348152437
+console.log(Math.tanh(1)); // 0.7615941559557649
+console.log(Math.asinh(1)); // 0.881373587019543
+console.log(Math.acosh(1)); // 0
+console.log(Math.atanh(0.5)); // 0.5493061443340549
+console.log(Math.log(2)); // 0.6931471805599453
+console.log(Math.log10(10)); // 1
+console.log(Math.log2(8)); // 3
+console.log(Math.exp(1)); // 2.718281828459045
+console.log(Math.expm1(1)); // 1.718281828459045
+console.log(Math.cbrt(8)); // 2
+console.log(Math.clz32(1)); // 31
+console.log(Math.imul(2, 3)); // 6
+console.log(Math.sign(-3)); // -1
+console.log(Math.abs(-3)); // 3
