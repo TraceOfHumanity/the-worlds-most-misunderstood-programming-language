016a188bb2124c17ce8547ede286487a488bd398
Alexander_Ovchar <azalexxx02@gmail.com>
2024-05-20 00:24:08 +0300

    math

---
+++ math.js
@@ -0,0 +1,10 @@
+console.log(Math.pow(2, 3)); // 8
+console.log(Math.sqrt(16)); // 4
+console.log(Math.random());
+console.log(Math.floor(3.9)); // 3
+console.log(Math.ceil(3.1)); // 4
+console.log(Math.round(3.5)); // 4
+console.log(Math.max(1, 2, 3, 4, 5)); // 5
+console.log(Math.min(1, 2, 3, 4, 5)); // 1
+console.log(Math.PI); // 3.141592653589793
+console.log(Math.E); // 2.718281828459045
