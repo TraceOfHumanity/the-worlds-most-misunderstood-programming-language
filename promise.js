e765185249e5ef329e97f58bddef521a9ed32681
Alexander_Ovchar <azalexxx02@gmail.com>
2024-10-29 22:23:25 +0200

    Promise

--- promise.js
+++ promise.js
@@ -1,7 +1,12 @@
-new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve("promise resolved");
-  }, 1000);
-}).then((data) => {
-  console.log(data);
-});
+// new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("promise resolved");
+//   }, 1000);
+// }).then((data) => {
+//   console.log(data);
+// });
+
+// fetch("https://jsonplaceholder.typicode.com/users")
+//   .then((response) => response.json())
+//   .then((json) => console.log(json))
+//   .catch((error) => console.log(error));
