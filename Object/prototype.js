8e10abf31ead0d03e6a24c378925d77b32464b3d
Alexander_Ovchar <azalexxx02@gmail.com>
2024-11-05 18:02:49 +0200

    String.prototype

--- Object/prototype.js
+++ Object/prototype.js
@@ -1,17 +1,17 @@
-function Car(brand, age) {
-  this.brand = brand;
-  this.age = age;
-}
+// function Car(brand, age) {
+//   this.brand = brand;
+//   this.age = age;
+// }
 
-Car.prototype.getBrand = function () {
-  return this.brand;
-};
+// Car.prototype.getBrand = function () {
+//   return this.brand;
+// };
 
-const toyota = new Car('Toyota', 10);
-const honda = new Car('Honda', 5);
+// const toyota = new Car('Toyota', 10);
+// const honda = new Car('Honda', 5);
 
-console.log(toyota.getBrand());
-console.log(honda.getBrand());
+// console.log(toyota.getBrand());
+// console.log(honda.getBrand());
 
 
 // class Car {
@@ -24,3 +24,9 @@ console.log(honda.getBrand());
 //     return this.brand;
 //   }
 // }
+
+const a = "qwerty";
+
+console.log(a.__proto__);
+
+console.log(a.__proto__ === String.prototype);
