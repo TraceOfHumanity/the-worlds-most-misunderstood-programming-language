9fbd280e263716f530b574f638420de9ce64d517
Alexander_Ovchar <azalexxx02@gmail.com>
2024-11-08 12:17:28 +0200

    tasksForTheInterview

--- Object/objectClone.js
+++ Object/objectClone.js
@@ -1,5 +1,6 @@
+//Shallow Copy
 let original = {name: "Alex", details: {age: 30}};
-let shallowCopy = { ...original };
+let shallowCopy = {...original};
 
 console.log(shallowCopy);
 
@@ -7,3 +8,34 @@ shallowCopy.details.age = 31;
 
 console.log(original);
 console.log(shallowCopy);
+
+//Deep Copy
+// let deepCopy = JSON.parse(JSON.stringify(original));
+
+// console.log(deepCopy);
+
+// deepCopy.details.age = 32;
+
+// console.log(original);
+// console.log(deepCopy);
+
+function deepClone(obj) {
+  if (obj === null || typeof obj !== 'object') return obj;
+  
+  let copy = Array.isArray(obj) ? [] : {};
+  
+  for (let key in obj) {
+      if (obj.hasOwnProperty(key)) {
+          copy[key] = deepClone(obj[key]);
+      }
+  }
+  return copy;
+}
+let deepCopy = deepClone(original);
+
+console.log(deepCopy);
+
+deepCopy.details.age = 32;
+
+console.log(original);
+console.log(deepCopy);
