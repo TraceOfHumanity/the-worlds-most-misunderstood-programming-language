39bf3b761f3379f5ef266afba85b1c1a16b398ef
Alexander_Ovchar <azalexxx02@gmail.com>
2024-11-09 16:53:32 +0200

    findMax

---
+++ tasksForTheInterview/findMax.js
@@ -0,0 +1,27 @@
+// function findMax(arr) {
+//   return Math.max(...arr)
+// }
+
+// console.log(findMax([1, 3, 7, 2, 5])); // Виведе: 7
+
+// function findMax(arr) {
+//   let max = 0;
+
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] > max) {
+//       max = arr[i];
+//     }
+//   }
+
+//   return max;
+// }
+
+// console.log(findMax([1, 3, 7, 2, 5])); // Виведе: 7
+
+// function findMax(arr) {
+//   const sortedArr = arr.sort((a, b) => a - b);
+//   console.log(sortedArr);
+//   return sortedArr[sortedArr.length - 1];
+// }
+
+// console.log(findMax([1, 3, 7, 2, 5])); // Виведе: 7
