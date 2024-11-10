103bf696da03c8156d32c72424f85f83b328ae9f
Alexander_Ovchar <azalexxx02@gmail.com>
2024-11-10 13:16:58 +0200

    memoization

--- tasksForTheInterview/fibonacci.js
+++ tasksForTheInterview/fibonacci.js
@@ -6,15 +6,23 @@
 
 // console.log(fibonacci(6)); // Виведе: 8
 
-// 2. iterative
-function fibonacci(n) {
-  let a = 0, b = 1, c;
-  for (let i = 2; i <= n; i++) {
-    c = a + b;
-    a = b;
-    b = c;
-  }
-  return b;
-}
+// // 2. iterative
+// function fibonacci(n) {
+//   let a = 0, b = 1, c;
+//   for (let i = 2; i <= n; i++) {
+//     c = a + b;
+//     a = b;
+//     b = c;
+//   }
+//   return b;
+// }
 
-console.log(fibonacci(6)); // Виведе: 8
+// console.log(fibonacci(6)); // Виведе: 8
+
+// 3. memoization
+function fibonacci(n, memo = {}) {
+  if (n in memo) return memo[n];
+  if (n <= 1) return n;
+  memo[n] = fibonacci(n - 1, memo) + fibonacci(n - 2, memo);
+  return memo[n];
+}
