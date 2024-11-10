2d1b0ded5f499f57de768d651190a10f509533b8
Alexander_Ovchar <azalexxx02@gmail.com>
2024-11-10 13:55:34 +0200

    uniqueArray

--- tasksForTheInterview/uniqueArray.js
+++ tasksForTheInterview/uniqueArray.js
@@ -1,5 +1,19 @@
+// function uniqueArray(arr) {
+//   return Array.from(new Set(arr));
+// }
+
+// console.log(uniqueArray([1, 2, 2, 3, 4, 4, 5])); // Виведе: [1, 2, 3, 4, 5]
+
 function uniqueArray(arr) {
-  return Array.from(new Set(arr));
+  const uniqArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr.lastIndexOf(arr[i]) === i) {
+      uniqArr.push(arr[i]);
+    }
+  }
+
+  return uniqArr;
 }
 
 console.log(uniqueArray([1, 2, 2, 3, 4, 4, 5])); // Виведе: [1, 2, 3, 4, 5]
