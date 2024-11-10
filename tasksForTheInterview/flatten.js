a118d95fcc760ae0183d3a253151a760a733d455
Alexander_Ovchar <azalexxx02@gmail.com>
2024-11-10 17:02:14 +0200

    flatten

---
+++ tasksForTheInterview/flatten.js
@@ -0,0 +1,15 @@
+const flatten = (arr) => {
+  var res = [];
+
+  for (var i = 0; i < arr.length; i++) {
+    if(Array.isArray(arr[i])){
+      res.push(...flatten(arr[i]));
+    } else {
+      res.push(arr[i]);
+    }
+  }
+
+  return res;
+};
+
+console.log(flatten([1, [2, 3], [[4, 5, [6, [7]]]]]));
