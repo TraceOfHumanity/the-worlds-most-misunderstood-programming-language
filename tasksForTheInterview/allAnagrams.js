d339b4d2a424a55f8515bcebd624adceac69ed08
Alexander_Ovchar <azalexxx02@gmail.com>
2024-11-14 20:09:28 +0200

    allAnagrams

---
+++ tasksForTheInterview/allAnagrams.js
@@ -0,0 +1,6 @@
+function allAnagrams(arr) {
+  const sorted = arr.map((str) => str.split("").sort().join(""));
+  return sorted.every((str) => str === sorted[0]);
+}
+
+console.log(allAnagrams(["abcd", "bdac", "cabd"]));
