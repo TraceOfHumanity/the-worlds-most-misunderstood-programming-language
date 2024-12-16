eb7f32e93d2fb4c566b2dfea34d179476c7f5529
Alexander_Ovchar <azalexxx02@gmail.com>
2024-12-16 11:13:34 +0200

    Remove unused files related to prime calculation and event loop examples
    - Deleted child-process.js, heavy.calc.js, constants.js, worker.js, and related event loop files to streamline the project.
    - This cleanup eliminates unnecessary code and focuses on the core functionality.

--- constants.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  PORT: 3636,
-  HOST: "localhost",
-  MULTIPLIER: 1000,
-  ITERATIONS: 400
-};
--- worker.js
+++
@@ -1,8 +0,0 @@
-const {parentPort} = require("worker_threads");
-const calculatePrimes = require("./heavy.calc");
-
-parentPort.on("message", ({multiplier, iterations}) => {
-  const result = calculatePrimes(iterations, multiplier);
-
-  parentPort.postMessage(result);
-});
