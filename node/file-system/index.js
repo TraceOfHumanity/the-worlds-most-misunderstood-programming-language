9eb8833d49f2cfc4d737030b7c537caf25d949ff
TraceOfHumanity <azalexxx02@gmail.com>
2026-01-18 14:48:03 +0200

    feat(Node): implement file command handling with create, delete, rename, and add functionalities

--- node/file-system/index.js
+++ node/file-system/index.js
@@ -9,20 +9,84 @@ const { buffer } = require('stream/consumers');
 //---------------------------------------------------------
 
 (async () => {
-    const commandFileHandler = await fs.open('./command.txt', 'r');
-    const watcher = fs.watch('./command.txt');
+    const CREATE_FILE = "create a file";
+    const DELETE_FILE = "delete the file";
+    const RENAME_FILE = "rename the file";
+    const ADD_TO_FILE = "add to the file";
 
+    const createFile = async (path) => {
+        try {
+            const existingFileHandle = await fs.open(path, 'r');
+            existingFileHandle.close();
+            return console.log(`this file ${path} already exists`);
+        } catch (error) {
+            const newFileHandle = await fs.open(path, 'w');
+            console.log("A new file has been created");
+            newFileHandle.close();
+        }
+    }
+
+    const deleteFile = async (path) => {
+        try {
+            await fs.unlink(path);
+            console.log(`file ${path} has been deleted`);
+        } catch (error) {
+            console.log(`file ${path} does not exist`);
+        }
+    }
+
+    const renameFile = async (oldPath, newPath) => {
+        console.log(`renaming file ${oldPath} to ${newPath}`);
+    }
+
+    const addToFile = async (path, content) => {
+        console.log(`adding content to file ${path}: ${content}`);
+    }
+
+
+
+    const commandFileHandler = await fs.open('./command.txt', 'r');
+
+    commandFileHandler.on("change", async () => {
+        const size = (await commandFileHandler.stat()).size;
+        const buffer = Buffer.alloc(size);
+        const offset = 0;
+        const length = buffer.byteLength;
+        const position = 0;
+
+        await commandFileHandler.read(buffer, offset, length, position);
+        const command = buffer.toString("utf-8");
+
+        if (command.includes(CREATE_FILE)) {
+            const path = command.substring(CREATE_FILE.length + 1);
+            await createFile(path);
+        }
+        if (command.includes(DELETE_FILE)) {
+            const path = command.substring(DELETE_FILE.length + 1);
+            await deleteFile(path);
+        }
+        if (command.includes(RENAME_FILE)) {
+            const _index = command.indexOf(" to ");
+            const oldPath = command.substring(RENAME_FILE.length + 1, _index);
+            const newPath = command.substring(_index + 4);
+
+            await renameFile(oldPath, newPath);
+        }
+        if (command.includes(ADD_TO_FILE)) {
+            const _index = command.indexOf(" this content: ");
+            const filePath = command.substring(ADD_TO_FILE.length + 1, _index);
+            const content = command.substring(_index + 15);
+
+            await addToFile(filePath, content);
+        }
+    })
+
+    const watcher = fs.watch('./command.txt');
     for await (const event of watcher) {
         if (event.eventType === 'change') {
-            console.log("This file has been changed");
-            const size = (await commandFileHandler.stat()).size;
-            const buffer = Buffer.alloc(size);
-            const offset = 0;
-            const length = buffer.byteLength;
-            const position = 0;
+            console.log("Command file has been changed");
+            commandFileHandler.emit("change");
 
-            const content = await commandFileHandler.read(buffer, offset, length, position);
-            console.log(content);
         }
     }
 })();
