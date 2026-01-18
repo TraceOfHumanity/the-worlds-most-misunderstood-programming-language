9990f80981ce100b9a3879b4d06261cabeb44678
TraceOfHumanity <azalexxx02@gmail.com>
2026-01-18 22:24:02 +0200

    feat(Node): implement encryption and decryption streams for file processing

--- node/streams/copy/copy.js
+++ node/streams/copy/copy.js
@@ -1,28 +1,54 @@
 const fs = require('node:fs/promises');
+const { pipeline } = require('node:stream');
+// (async () => {
+//     console.time("copy");
+
+//     const sourcefile = await fs.open('src.txt', 'r');
+//     const destFile = await fs.open('dest.txt', 'w');
+
+//     let bytesRead = -1;
+
+//     while (bytesRead !== 0) {
+//         const readResult = await sourcefile.read()
+//         bytesRead = readResult.bytesRead
+
+//         console.log(bytesRead)
+//         if (bytesRead !== 16384) {
+//             const indexOfNotFilled = readResult.buffer.indexOf(0);
+//             const newBuffer = Buffer.alloc(indexOfNotFilled);
+//             readResult.buffer.copy(newBuffer, 0, 0, indexOfNotFilled);
+//             destFile.write(newBuffer);
+//         } else {
+//             destFile.write(readResult.buffer);
+//         };
+
+//     }
+
+//     console.timeEnd("copy");
+// })()
+
+// ------------------------------------------------------------
+
 
 (async () => {
     console.time("copy");
 
-    const source = await fs.open('src.txt', 'r');
+    const sourcefile = await fs.open('src.txt', 'r');
     const destFile = await fs.open('dest.txt', 'w');
 
-    let bytesRead = -1;
+    const readStream = sourcefile.createReadStream();
+    const writeStream = destFile.createWriteStream();
 
-    while (bytesRead !== 0) {
-        const readResult = await source.read()
-        bytesRead = readResult.bytesRead
+    // readStream.pipe(writeStream);
+    // readStream.on("end", () => {
+    //     console.timeEnd("copy");
+    // })
 
-        console.log(bytesRead)
-        if (bytesRead !== 16384) {
-            const indexOfNotFilled = readResult.buffer.indexOf(0);
-            const newBuffer = Buffer.alloc(indexOfNotFilled);
-            readResult.buffer.copy(newBuffer, 0, 0, indexOfNotFilled);
-            destFile.write(newBuffer);
-        } else {
-            destFile.write(readResult.buffer);
-        };
+    await pipeline(readStream, writeStream, (err) => {
+        if (err) {
+            console.log(err);
+        }
+        console.timeEnd("copy");
+    });
 
-    }
-
-    console.timeEnd("copy");
 })()
