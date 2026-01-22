de1c525c4c772991587edfa12719823a98f03a3d
TraceOfHumanity <azalexxx02@gmail.com>
2026-01-22 22:21:33 +0200

    feat(Node): enhance Butter class with middleware support and additional routing for static files

--- node/http/web/butter.js
+++ node/http/web/butter.js
@@ -4,20 +4,11 @@ const fs = require("node:fs/promises");
 class Butter {
   constructor() {
     this.server = http.createServer();
-    /**
-     * {
-     *  "get/": () => { ... },
-     *  "post/upload": () => { ... }
-     * }
-     *
-     *
-     * this.routes["get/"]()
-     *
-     */
+
     this.routes = {};
+    this.middleware = [];
 
     this.server.on("request", (req, res) => {
-      // Send a file back to the client
       res.sendFile = async (path, mime) => {
         const fileHandle = await fs.open(path, "r");
         const fileStream = fileHandle.createReadStream();
@@ -27,12 +18,42 @@ class Butter {
         fileStream.pipe(res);
       };
 
-      this.routes[req.method.toLocaleLowerCase() + req.url](req, res);
+      res.status = (code) => {
+        res.statusCode = code;
+        return res;
+      };
+
+      res.json = (data) => {
+        res.setHeader("Content-Type", "application/json");
+        res.end(JSON.stringify(data));
+      };
+
+      const runMiddleware = (req, res, middleware, index) => {
+        if (index === middleware.length) {
+          if (!this.routes[req.method.toLocaleLowerCase() + req.url]) {
+            return res
+              .status(404)
+              .json({ error: `Cannot ${req.method} ${req.url}` });
+          }
+
+          this.routes[req.method.toLowerCase() + req.url](req, res);
+        } else {
+          middleware[index](req, res, () => {
+            runMiddleware(req, res, middleware, index + 1);
+          });
+        }
+      };
+
+      runMiddleware(req, res, this.middleware, 0);
     });
   }
 
   route(method, path, cb) {
     this.routes[method + path] = cb;
+  }
+
+  beforeEach(cb) {
+    this.middleware.push(cb);
   }
 
   listen(port, cb) {
