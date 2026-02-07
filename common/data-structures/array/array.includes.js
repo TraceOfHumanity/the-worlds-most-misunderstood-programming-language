9a2fe7ad1f06bfc296f5cfddbc4ef890fdcbede8
TraceOfHumanity <azalexxx02@gmail.com>
2026-02-07 16:40:23 +0200

    feat(Array): add includes and indexOf methods with implementation examples and explanations

---
+++ common/data-structures/array/array.includes.js
@@ -0,0 +1,10 @@
+// includes метод перевіряє, чи містить масив заданий елемент
+// повертає true або false
+
+const array = [1, 2, 3, 4, 5];
+console.log(array.includes(3));
+
+//--------------------------------
+
+const array1 = [1, 2, 3, 4, 5];
+console.log(array1.includes(6));
